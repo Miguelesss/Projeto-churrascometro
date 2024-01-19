22262e582c7a1dc78dc1b82946430ec05140e0b1
Miguel Angelo <134246672+Miguelesss@users.noreply.github.com>
2024-01-19 15:04:42 -0300

    primeiros passos

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var nA;
+var nC;
+var nD; 
+
+function numbA(a){
+    nA = a * 0.5;
+    return nA;
+}
+function numbC(c){
+    nC = c * 0.5;
+    return nC;
+}
+function numbD(d){
+    nD = d * 0.5;
+    return nD;
+}
+
+

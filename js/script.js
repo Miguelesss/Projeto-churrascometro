8bf11bc227014e0ed0c44e9c58448b50ce99cf4a
Miguel Angelo <134246672+Miguelesss@users.noreply.github.com>
2024-01-19 20:38:22 -0300

    estrutura básica front-back

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 var nA;
 var nC;
-var nD; 
-
+var nD;
+var cerv;
+var beb;
+var carne;
 function numbA(a){
-    nA = a * 0.5;
+    nA = a;
     return nA;
 }
 function numbC(c){
-    nC = c * 0.5;
+    nC = c;
     return nC;
 }
 function numbD(d){
-    nD = d * 0.5;
+    nD = d;
     return nD;
+}
+function calc(){
+    cerv = (nA * 2); 
+    beb = (nC * 0.5);
+    carne = (nA * 0.5) + (nC * 0.25);
+    console.log((cerv * nD) + " " + (beb * nD) + " " + (carne * nD))
+    
 }
 
 
